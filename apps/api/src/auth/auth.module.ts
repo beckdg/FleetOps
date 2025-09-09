22ec5eefb6c29a2fcecf61dabdface4faa56263b
beckdg <bekanderesa27@gmail.com>
2025-09-09 10:42:00 +0000

    chore(auth): update auth

---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+/**
+ * Authentication module scaffold.
+ * JWT, session, and credential flows will be implemented here.
+ */
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class AuthModule {}
