0cfc86f5579d9811a253509e4397d689b24dfe5d
beckdg <bekanderesa27@gmail.com>
2025-09-15 15:17:00 +0000

    chore(roles): update roles

---
+++ apps/api/src/roles/roles.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+/**
+ * Roles module scaffold.
+ * RBAC, permissions, and role assignments will be implemented here.
+ */
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class RolesModule {}
