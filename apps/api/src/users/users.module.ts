8f1a43e23f315bfd5b324d612a860e8506af1ae8
beckdg <bekanderesa27@gmail.com>
2025-09-18 13:50:00 +0000

    chore(users): update users

---
+++ apps/api/src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+/**
+ * Users module scaffold.
+ * User management and profile operations will be implemented here.
+ */
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class UsersModule {}
