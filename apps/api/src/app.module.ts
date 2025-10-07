98786c5889142ed737a0ba1c9ad2bd5a4253d5c6
beckdg <bekanderesa27@gmail.com>
2025-10-07 11:21:00 +0000

    chore(app.module.ts): update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
 import { envValidationSchema } from './shared/constants/env.validation';
@@ -23,6 +24,7 @@ import { envValidationSchema } from './shared/constants/env.validation';
     }),
     DatabaseModule,
     HealthModule,
+    OrganizationsModule,
     AuthModule,
     UsersModule,
     RolesModule,
