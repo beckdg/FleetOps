d64f7983a20d3a3bdc2d5672801e57b8339b7848
beckdg <bekanderesa27@gmail.com>
2025-10-24 16:24:00 +0000

    chore(tests): update tests api

--- apps/api/test/setup-e2e.ts
+++ apps/api/test/setup-e2e.ts
@@ -2,4 +2,4 @@ process.env.NODE_ENV = 'test';
 process.env.PORT = '3000';
 process.env.DATABASE_URL =
   process.env.DATABASE_URL ??
-  'postgresql://fleetops:fleetops@localhost:5432/fleetops_test?schema=public';
+  'postgresql://fleetops:fleetops@localhost:5433/fleetops_test?schema=public';
