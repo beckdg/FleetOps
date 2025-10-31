611e38596e26a53845be2dac7dbd4cba1754ec5c
beckdg <bekanderesa27@gmail.com>
2025-10-31 09:51:00 +0000

    feat(auth): add auth guards

---
+++ apps/api/src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,26 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+
+import { IS_PUBLIC_KEY } from '../../shared/decorators/public.decorator';
+import { JWT_STRATEGY_NAME } from '../constants/auth.constants';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard(JWT_STRATEGY_NAME) {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}
