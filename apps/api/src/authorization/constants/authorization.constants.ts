eed7eb1b07c19bc361c77e50677444589db19b59
beckdg <bekanderesa27@gmail.com>
2025-11-17 12:28:00 +0000

    feat(authorization): add authorization constants

--- apps/api/src/authorization/constants/authorization.constants.ts
+++ apps/api/src/authorization/constants/authorization.constants.ts
@@ -1,6 +1,6 @@
 /** Organization role that bypasses permission checks. */
 export const ADMIN_ROLE_NAME = 'admin';
 
-export const REQUIRED_PERMISSION_KEY = 'requiredPermission';
+export const REQUIRED_PERMISSIONS_KEY = 'requiredPermissions';
 
 export const ORGANIZATION_SCOPE_PARAM_KEY = 'organizationScopeParam';
