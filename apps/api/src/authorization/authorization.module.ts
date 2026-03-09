57fba78085244e7b471379f2d6be233658b956d8
beckdg <bekanderesa27@gmail.com>
2026-03-09 12:41:00 +0000

    feat(authorization): add authorization

--- apps/api/src/authorization/authorization.module.ts
+++ apps/api/src/authorization/authorization.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 
+import { AuditStoreModule } from '../operations/audit/audit-store.module';
+import { RequestContextModule } from '../operations/request-context/request-context.module';
 import { PermissionsModule } from '../permissions/permissions.module';
 import { RolesModule } from '../roles/roles.module';
 import { PermissionGuard } from './guards/permission.guard';
 import { AuthorizationAuditService } from './services/authorization-audit.service';
 
 @Module({
-  imports: [PermissionsModule, RolesModule],
+  imports: [PermissionsModule, RolesModule, AuditStoreModule, RequestContextModule],
   providers: [
     AuthorizationAuditService,
     PermissionGuard,
