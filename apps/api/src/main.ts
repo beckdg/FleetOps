fe7ac0b72fa1b0f3217965703dea4627254751e6
beckdg <bekanderesa27@gmail.com>
2026-03-16 12:55:00 +0000

    feat(main.ts): add main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
@@ -9,9 +10,11 @@ import { API_GLOBAL_PREFIX } from './shared/constants/app.constants';
 import { EnvironmentVariables } from './shared/constants/env.validation';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bufferLogs: true,
   });
+
+  app.set('trust proxy', true);
 
   const configService = app.get(ConfigService<EnvironmentVariables, true>);
   const port = configService.get('PORT', { infer: true });
